95608d298b8e4c94edb9ce51e447f9655afe7da3
Michal Katuščák <michal@katuscak.cz>
2017-02-01 20:13:17 +0100

    Remove bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,2 @@
 <?php
 
-define("KATUSCAK_DIR", __DIR__);
-define("KATUSCAK_DIR_CONFIG", KATUSCAK_DIR . "/config");
-
-include_once __DIR__ . "/autoload.php";
--- index.php
+++ index.php
@@ -1,15 +1,21 @@
 <?php
-error_reporting(E_ALL);
-ini_set("display_errors", true);
 
 use Katuscak\Kernel\Factory;
 use Katuscak\Component\Routing\Router;
 use Katuscak\Component\Routing\Route;
+use App\Controller\HomepageController;
+use App\Controller\LoginController;
 
-include_once __DIR__ . "/bootstrap.php";
+// Step 1 - Set autoloading
+include_once __DIR__ . "/autoload.php";
 
+// Step 2 - Set constants
+define("KATUSCAK_DIR", __DIR__);
+define("KATUSCAK_DIR_CONFIG", KATUSCAK_DIR . "/config");
+
+// Step 3 - Set routes
 $router = Factory::load(Router::class);
 $router->addRoute(
-    new Route("homepage", "/", [\App\Controller\HomepageController::class, "index"]),
-    new Route("login", "/login", [\App\Controller\LoginController::class, "index"])
+    new Route("homepage", "/", [HomepageController::class, "index"]),
+    new Route("login", "/login", [LoginController::class, "index"])
 );
